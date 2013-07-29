acc1cc839396bb44f9a4f1ab3ec9e6793454c59b
Maksym Bondarenko <mabondaren@yandex.ru>
2013-07-29 22:41:39 +0300

    Very cool thing was implemented

--- web.js
+++ web.js
@@ -1,13 +1,28 @@
 var fs = require('fs');
 var express = require('express');
+var io = require('socket.io');
+var http = require('http');
 
 var app = express();
+var server = http.createServer(app);
+var io = io.listen(server);
+
+io.sockets.on('connection', function(socket){
+	console.log('Client connected...');
+	socket.on('log_in', function(data){
+		console.log('Name: ' + data.user_name + ' Password: ' + data.password);
+	});
+	//client.emit('messages', {hello: 'world'});
+});
 
 app.get('/', function(request, response) {
   response.sendfile(__dirname + '/login.html');
 });
 
+//Make css, js and other files viewable by html file
+app.use(express.static(__dirname));
+
 var port = process.env.PORT || 8080;
-app.listen(port, function() {
+server.listen(port, function() {
   console.log("Listening on " + port);
 });
