452a6a633ddb47ebd6ba5a3fd47d35fd0a1e1f61
Maksym Bondarenko <mabondaren@yandex.ru>
2013-08-18 20:12:24 +0300

    Chapter

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.configure(function(){
+  app.set('view engine', 'jade');
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/', function(req, res){
+	res.render(__dirname + '/public/views/index.jade', {layout:false});
+});
+
+app.listen(8080);
