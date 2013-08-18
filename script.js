69a3d14b0470cc7656585d97a57d3b0f7b7e46ef
Vlad Grytsun <vlg92@mail.ru>
2013-08-18 13:34:12 +0300

    dependencies settings

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-/*function SendToServer() {
-	alert("S");
-	//var server = io.connect('http://localhost:8080');
-	server.emit('message', "Yeah!");
-}*/
-
 window.onload = function(){
 	var socket = io.connect('http://localhost:8080');
 	socket.on('connect', function(){
-		alert("A");
 		document.getElementById('sign_in').onclick = function() {
 			socket.emit('log_in', {
 				user_name: document.getElementById('user_name').value,
