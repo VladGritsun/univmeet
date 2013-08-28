6d518b6c4586dda65a3114024792d81e48f56915
Maksym Bondarenko <mabondaren@yandex.ru>
2013-08-28 12:17:31 +0300

    Started ch 08

--- app.js
+++ app.js
@@ -163,6 +163,15 @@ app.post('/resetPassword', function(req,res){
 	res.render('resetPasswordSuccess.jade');
 });
 
+app.get('/accounts/:id/contacts', function(req, res){
+	var accountId = req.params.id == 'me'
+						? req.session.accountId
+						: req.params.id;
+	models.Account.findById(accountId, function(account){
+		res.send(account.contacts);
+	});
+});
+
 app.listen(8080, function() {
 	console.log("I'm listening!");
 });
